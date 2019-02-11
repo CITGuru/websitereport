baee0dc88b888697d4cf67e771001aed64b9b325
Oyetoke Toby <oyetoketoby80@gmail.com>
2019-02-11 17:26:07 +0100

    final push

---
+++ websitereport/settings/prod.py
@@ -0,0 +1,12 @@
+from base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'websitereport',
+        'USER': 'admin',
+        'PASSWORD': 'blicnet1*',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
